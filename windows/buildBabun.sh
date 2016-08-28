2c5493c0ae43650bdf6b6f11ef0a93ba3ff0affd
chenwenbo <594017622@qq.com>
2016-08-28 21:32:57 +0800

    add cp fonts script

--- windows/buildBabun.sh
+++ windows/buildBabun.sh
@@ -9,3 +9,4 @@ cp .zshrc ~/
 cp .tmux.conf ~/
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+cp fonts/*.ttf /cygdrive/c/Windows/Fonts
