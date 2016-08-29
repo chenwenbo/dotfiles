743772b46999eabda9b74bb5d9d4c0c782776b48
chenwenbo <594017622@qq.com>
2016-08-29 14:08:39 +0800

    update sh

--- windows/buildBabunEnv.sh
+++ windows/buildBabunEnv.sh
@@ -1,9 +1,21 @@
 #!/bin/zsh
 wget https://bootstrap.pypa.io/get-pip.py -O - | python
+echo 'install tmux'
 pact install tmux
+echo 'cp .minttyrc'
 cp .minttyrc ~/
+echo 'cp vimrc'
 cp vimrc /etc/vimrc
+echo 'cp .zshrc'
 cp .zshrc ~/
+echo 'cp .tmux.conf'
 cp .tmux.conf ~/
+echo 'install plugin zsh-autosuggestions'
 git clone git://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+echo 'install plugin zsh-syntax-highlighting'
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+echo 'install plugin autojump'
+git clone git://github.com/joelthelion/autojump.git
+cd autojump
+./install.py
+echo 'end'
