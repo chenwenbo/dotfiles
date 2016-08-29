8d81fb37919e327e38793204e9854879326bc52e
chenwenbo <594017622@qq.com>
2016-08-29 11:09:10 +0800

    update buildBabunEnv.sh

--- windows/buildBabunEnv.sh
+++ windows/buildBabunEnv.sh
@@ -1,11 +1,7 @@
 #!/bin/zsh
 echo "install common app"
 wget https://bootstrap.pypa.io/get-pip.py -O - | python
-pip install mackup
 pact install tmux
-pact install pip
-pact install zip
-pact install unzip
 cp .minttyrc ~/
 cp vimrc /etc/vimrc
 cp .zshrc ~/
